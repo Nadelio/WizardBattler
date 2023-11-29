53c5dd89544a75d95f6da6da06794d20ad6f7859
Nadelio <138712393+Nadelio@users.noreply.github.com>
2023-11-29 16:05:30 -0600

    Added "name" String variable to Spell.java

--- Spell.java
+++ Spell.java
@@ -5,17 +5,20 @@ public class Spell
     private boolean hasEffect;
     private int damage;
     private String type;
+    private String name;
     
-    public Spell(boolean hasEffect, int damage, String type)
+    public Spell(boolean hasEffect, int damage, String type, String name)
     {
         this.hasEffect = hasEffect;
         this.damage = damage;
         this.type = type;
+        this.name = name;
     }
 
     public boolean getHasEffect(){return hasEffect;}
     public int getSpellDamage(){return damage;}
     public String getType(){return type;}
+    public String getName(){return name;}
 
     public void effectProcess(Enemy currentEnemy){}
 }
