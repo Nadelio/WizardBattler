e31ffb50828207323e87984412ab18f30712f67c
Nadelio <carefulsniffle@gmail.com>
2023-12-06 15:12:45 -0600

    Add events class with notes on possibles

---
+++ Game/Events.java
@@ -0,0 +1,15 @@
+package Game;
+
+public class Events
+{
+    // add a bool for every event in the game, then we want to have a cycler that can do something depending on the event
+    // or we can have a data type called Event, that is a bool and a string
+    
+    
+    // Event data type:
+    // private boolean isActive;
+    // private String eventName;
+    // if(isActive){Events.doEvent(eventName);}
+    
+    // maybe??
+}
