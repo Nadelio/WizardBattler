8202b473d57285873c12af400a4c1c2469965a44
Nadelio <carefulsniffle@gmail.com>
2024-01-18 17:54:05 -0600

    Working on Polishing something that won't be in the final game.

--- Classes/EntityClass.java
+++ Classes/EntityClass.java
@@ -1,13 +1,31 @@
 package Classes;
 
+import java.util.ArrayList;
+
 public class EntityClass
 {
+    public static ArrayList<String> classList = new ArrayList<String>(){{add("Fighter"); add("Wizard"); add("Archer"); add("Paladin");}};
+
     public enum Classes
     {
-        Fighter,
-        Wizard,
-        Archer,
-        Paladin;
+        Fighter(0),
+        Wizard(1),
+        Archer(2),
+        Paladin(3);
+
+        private int classIndex;
+
+        private Classes(int index){this.classIndex = index;}
+
+        public static Classes getClass(int index)
+        {
+            for(Classes c : Classes.values())
+            {
+                if(c.classIndex == index){return c;}
+            }
+            throw new IllegalArgumentException("Index does not represent a existing class, please retry");
+        }
+
     } // add more classes in the future?
 }
 // Fighter: Done
--- Game/Main.java
+++ Game/Main.java
@@ -1,7 +1,6 @@
 package Game;
 
-import java.io.IOError;
-import java.io.IOException;
+import java.util.Scanner;
 
 import Classes.EntityClass;
 import Weapons.BasicSword;
@@ -25,11 +24,51 @@ public class Main
         System.out.println();
     }
 
+    private static int classChoice;
+
+    public static EntityClass.Classes chooseClass()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please choose a class by entering the corresponding number!");
+        printClasses();
+        classChoice = in.nextInt();
+        return EntityClass.Classes.getClass(classChoice);
+    }
+
+    public static void printClasses()
+    {
+        int classValue = 0;
+
+        for(EntityClass.Classes c : EntityClass.Classes.values())
+        {
+            System.out.print(classValue);
+            System.out.print(" : " + EntityClass.classList.get(classValue) + "\n");
+            classValue++;
+        }
+    }
+
     public static void main(String[] args) 
     {
         gameStartAnim();
+         
+        // init weapons
         new Weapons();
-        player = new Player(10, 0, new BasicSword(), 1, 1, "NONE", EntityClass.Classes.Fighter, "test_player");
+
+        // set player's name and class
+        System.out.println("Please type your name!");
+        Scanner playerInput = new Scanner(System.in);
+        String playerName = playerInput.nextLine();
+
+        EntityClass.Classes playerClass = chooseClass();
+
+        //Welcome the player
+        System.out.println("Welcome " + playerName + "!\nYou have chosen the class " + EntityClass.classList.get(classChoice) + "!");
+
+        // create the player
+        player = new Player(10, 0, new BasicSword(), 1, 1, "NONE", playerClass, playerName);
+        
+        System.out.println("-----------------------------------------------------");
+
         new GenerateFight();
     }
 }
